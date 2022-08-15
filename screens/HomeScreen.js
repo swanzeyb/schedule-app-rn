10431a8a866eafff4a8251b64fb4b3d4a4f36033
Ben <swanzeyb2001@gmail.com>
2022-08-15 15:44:50 -0700

    Add RN Firebase

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,28 @@
+import { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { View, Image } from 'react-native'
+import { View, Image, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Button, Text } from '../components'
 import tw from '../tw'
 
 const LogoSrc = require('../assets/logo.png')
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation: { navigate } }) {
+  const [isAuthed] = useState(false)
+
+  const signIn = () => navigate('Camera')
+
+  const addSchedule = () => {
+    if (!isAuthed) {
+      Alert.alert(
+        'Sign In',
+        'To continue, please sign in with the Google account tied to your calendar.', [
+        { text: 'CANCEL', style: 'cancel' },
+        { text: 'SIGN IN', onPress: signIn }
+      ])
+    }
+  }
+
   return (
     <SafeAreaView style={tw`flex-1`}>
       <View style={tw`flex-1 py-16 items-center justify-between`}>
@@ -17,7 +33,7 @@ export default function HomeScreen() {
           <Text style={tw`text-sm pt-1.5 text-center`}>Easily add your work schedule to{'\n'}Google Calendar</Text>
         </View>
         <Button
-          // onPress={addSchedule}
+          onPress={addSchedule}
           title="Add Schedule"
         />
       </View>
