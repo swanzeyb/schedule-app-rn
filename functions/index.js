d937adfc3e480400f9f0c02294092ec403411340
Ben <swanzeyb2001@gmail.com>
2022-08-18 22:39:36 -0700

    Detect text in images

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions')
 const vision = require('@google-cloud/vision')
 
+// Instantiate a vision client
+const client = new vision.ImageAnnotatorClient()
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -9,17 +12,41 @@ const vision = require('@google-cloud/vision')
 //   response.send('Hello from Firebase!')
 // })
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info(context.auth.token?.firebase?.sign_in_provider, {structuredData: true})
-  if (context.auth.token?.firebase?.sign_in_provider !== 'google.com') {
-    throw new functions.https.HttpsError(
-      'unauthenticated',
-      'annotateImage must be called while authenticated.'
-    )
+exports.helloWorld = functions.https.onCall(async (data, ctx) => {
+  if (!ctx.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
   }
-  try {
-    response.send('Hello from Firebase!')
-  } catch (e) {
-    throw new functions.https.HttpsError('internal', e.message, e.details)
+
+  const results = await client.annotateImage({
+    image: {
+      content: data.imgBase64,
+    },
+    features: [{type: 'DOCUMENT_TEXT_DETECTION'}],
+  })
+
+  const pages = results[0]?.fullTextAnnotation?.pages
+  if (pages?.length > 0) {
+    const text = []
+    for (const [_, page] of pages.entries()) {
+      for (const [_, block] of page.blocks.entries()) {
+        let str = ''
+        for (const [i, paragraph] of block.paragraphs.entries()) {
+          for (const [_, word] of paragraph.words.entries()) {
+            let wrd = ''
+            for (const [_, symbol] of word.symbols.entries()) {
+              wrd += symbol.text
+            }
+            str += wrd.length === 1 ? wrd : ` ${wrd}`
+          }
+          const paraLength = block.paragraphs.length
+          if (i == paraLength) word += paraLength > 1 ? ' ' : ''
+        }
+        str = str.trim()
+        if (str.length > 1) text.push(str)
+      }
+    }
+    return { text }
   }
+
+  return { text: [] }
 })
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { StatusBar } from 'expo-status-bar'
 import { View, Image, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { launchImageLibraryAsync, MediaTypeOptions } from 'expo-image-picker'
+import functions from '@react-native-firebase/functions'
+import { useEffect } from 'react'
 import { Button, Text } from '../components'
 import { useGoogleSignIn } from '../hooks'
 import tw from '../tw'
@@ -32,31 +34,48 @@ export default function HomeScreen({ navigation: { navigate } }) {
       })
   }
 
-  const pickImage = () => {
-    return launchImageLibraryAsync({
-      mediaTypes: MediaTypeOptions.Images,
-      allowsEditing: true,
-      quality: 1,
-    })
-      .then((result) => {
-        if (!result.cancelled) {
-          const form = new FormData()
-          form.append('any', { uri: result.uri, name: 'schedule.jpg', type: 'image/jpeg' })
-          form.append('viewport', JSON.stringify({ width: result.width, height: result.height }))
-          fetch('http://192.168.0.115:1449/text', {
-            method: 'post',
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-            body: form
-          }).then(async response => {
-            const value = await response.json()
-            console.log(`\n${JSON.stringify(value['text'], null, 2)}\n`)
-          }).catch(err => {
-            console.log(err)
-          })
-        }
+  const pickImage = async () => {
+    const fns = functions() //'us-central1'
+    fns.useEmulator('100.114.76.77', 5001)
+    const helloWorld = fns.httpsCallable('helloWorld')
+
+    const base64 = await launchImageLibraryAsync({
+        mediaTypes: MediaTypeOptions.Images,
+        allowsEditing: false,
+        base64: true,
+        quality: 1,
+      }).then(result => {
+        if (!result.cancelled) return result.base64
       })
+
+    helloWorld({ imgBase64: base64 })
+      .then(({ data }) => console.log(data))
+      .catch(err => console.log(err))
+    // return launchImageLibraryAsync({
+    //   mediaTypes: MediaTypeOptions.Images,
+    //   allowsEditing: true,
+    //   quality: 1,
+    // })
+    //   .then((result) => {
+    //     if (!result.cancelled) {
+    //       // const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
+    //       const form = new FormData()
+    //       form.append('any', { uri: result.uri, name: 'schedule.jpg', type: 'image/jpeg' })
+    //       form.append('viewport', JSON.stringify({ width: result.width, height: result.height }))
+    //       fetch('http://192.168.0.115:1449/text', {
+    //         method: 'post',
+    //         headers: {
+    //           'Content-Type': 'multipart/form-data',
+    //         },
+    //         body: form
+    //       }).then(async response => {
+    //         const value = await response.json()
+    //         console.log(`\n${JSON.stringify(value['text'], null, 2)}\n`)
+    //       }).catch(err => {
+    //         console.log(err)
+    //       })
+    //     }
+    //   })
   }
 
   const addSchedule = () => {
